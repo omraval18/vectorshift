730da5f9589069dd3dd2e40ab79de5eece822ce9
omraval18 <r.auckwell18@gmail.com>
2024-06-28 01:23:19 +0530

    added dropdown to header and added grid and more links to sheet and increased it's width

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,24 @@
-import { CircleUser, Menu, Package2 } from "lucide-react";
+"use client";
+import { ArrowDown, ChevronDown, CircleUser, Menu, Package2 } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
+    Menubar,
+    MenubarCheckboxItem,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarRadioGroup,
+    MenubarRadioItem,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarSub,
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarTrigger,
+} from "@/components/ui/menubar";
 import ShimmerButton from "../shimmer-button";
 import Image from "next/image";
 
@@ -34,12 +42,25 @@ export default function Header() {
                         <span className="text-xs md:text-sm font-medium">VectorShift</span>
                     </Link>
                     <div className="hidden md:flex font-bricolage items-center gap-4 text-sm font-normal md:text-sm">
-                        <Link
-                            href="#"
-                            className="text-muted-foreground transition-colors hover:text-foreground"
-                        >
-                            Platform
-                        </Link>
+                        <Menubar className="border-none bg-transparent p-0 ">
+                            <MenubarMenu>
+                                <MenubarTrigger className="border-none p-0 data-[state=open]:bg-transparent">
+                                    <span className="text-muted-foreground font-normal flex items-center  transition-colors hover:text-foreground">
+                                        <p>Platform</p>
+                                        <ChevronDown className="size-4" />
+                                    </span>
+                                </MenubarTrigger>
+                                <MenubarContent className="mt-4 bg-background/70 backdrop-blur-3xl -webkit-backdrop-blur-lg border border-muted">
+                                    <MenubarItem>Pipeline</MenubarItem>
+                                    <MenubarItem>Marketplace</MenubarItem>
+                                    <MenubarItem>Evaluations</MenubarItem>
+                                    <MenubarItem>Chat</MenubarItem>
+                                    <MenubarItem>Search</MenubarItem>
+                                    <MenubarItem>Agents</MenubarItem>
+                                    <MenubarItem>Automation</MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+                        </Menubar>
                         <Link
                             href="#"
                             className="text-muted-foreground transition-colors hover:text-foreground"
@@ -88,9 +109,12 @@ export default function Header() {
                             <span className="sr-only">Toggle navigation menu</span>
                         </Button>
                     </SheetTrigger>
-                    <SheetContent side="left">
-                        <nav className="grid gap-6 text-lg font-medium">
-                            <Link href="#" className="flex items-center gap-2 text-lg font-medium">
+                    <SheetContent side="left" className="w-[90%]">
+                        <nav className="grid grid-cols-2 gap-6 text-lg font-medium font-bricolage">
+                            <Link
+                                href="#"
+                                className="flex items-center gap-2 text-lg font-medium col-span-2"
+                            >
                                 <Image
                                     src={
                                         "https://ik.imagekit.io/omraval18/download%20(31)_w4GODqvey4.png?updatedAt=1719134003161"
@@ -102,7 +126,25 @@ export default function Header() {
                                 <span className="text-xs md:text-sm font-medium">VectorShift</span>
                             </Link>
                             <Link href="#" className="text-muted-foreground hover:text-foreground">
-                                Product
+                                Pipeline
+                            </Link>
+                            <Link href="#" className="text-muted-foreground hover:text-foreground">
+                                Chat
+                            </Link>
+                            <Link href="#" className="text-muted-foreground hover:text-foreground">
+                                Marketplace
+                            </Link>
+                            <Link href="#" className="text-muted-foreground hover:text-foreground">
+                                Agents
+                            </Link>
+                            <Link href="#" className="text-muted-foreground hover:text-foreground">
+                                Automation
+                            </Link>
+                            <Link href="#" className="text-muted-foreground hover:text-foreground">
+                                Search
+                            </Link>
+                            <Link href="#" className="text-muted-foreground hover:text-foreground">
+                                Evaluations
                             </Link>
                             <Link href="#" className="text-muted-foreground hover:text-foreground">
                                 Enterprise
@@ -113,7 +155,7 @@ export default function Header() {
                             <Link href="#" className="text-muted-foreground hover:text-foreground">
                                 Resources
                             </Link>
-                            <Link href="#" className="hover:text-foreground">
+                            <Link href="#" className="text-muted-foreground hover:text-foreground">
                                 Discord
                             </Link>
                         </nav>
